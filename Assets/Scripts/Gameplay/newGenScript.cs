4da77943bd7c797dd1ea43688a1bfedf0385867d
Orange-Utan <83580125+Orange-Utan@users.noreply.github.com>
2021-05-08 16:13:19 +0200

    newGenScript
    weiter gearbeitet; Bug gefunden: Abstand wann die Objekte Spawnen ist teilweise falsch

--- Assets/Scripts/Gameplay/newGenScript.cs
+++ Assets/Scripts/Gameplay/newGenScript.cs
@@ -4,27 +4,58 @@ using UnityEngine;
 
 public class newGenScript : MonoBehaviour
 {
-	private int countFram;
+	private GameObject _mainObj;
+	private Vars _mainVars;
+	private float _spawnCount;
+	private float _spawnBorder;
+	private string _enemyName;
+	private GameObject _Spawn;
+	public string[] EnemyArray;
+
+	[Header("Enemys")]
 	public GameObject eGan;
-	private GameObject Spawn;
+	public GameObject eIQ;
+	public GameObject eLof;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _mainObj = GameObject.FindWithTag("Main");
+        _mainVars = _mainObj.GetComponent<Vars>();
+        _spawnBorder = 300f;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        countFram++;
-        if(countFram >= 100) {
-        	s();
-        	countFram = 0;
+        _spawnCount = _spawnCount + (100*Time.fixedDeltaTime);
+
+        if(_spawnCount >= _spawnBorder) {
+        	SpawnFunction(Random.Range(0, 3));
+        	_spawnCount = 0;
         }
         
     }
 
-    void s() {
-    	Spawn = Instantiate(eGan, transform.position, Quaternion.identity) as GameObject;
+    void SpawnFunction(int whichOne) {
+
+    	_enemyName = EnemyArray[whichOne];
+    	
+    	switch (_enemyName) {
+    		case "gan":
+    			_Spawn = Instantiate(eGan, transform.position, Quaternion.identity) as GameObject;
+    			break;
+    		case "iq":
+    			_Spawn = Instantiate(eIQ, transform.position, Quaternion.identity) as GameObject;
+    			break;
+    		case "lof":
+    			_Spawn = Instantiate(eLof, transform.position, Quaternion.identity) as GameObject;
+    			break;
+
+    	}
+
+    	Debug.Log(_Spawn);
+    	_spawnBorder = 300f - (_mainVars.scoreInt / 100);
+    	Debug.Log("nextBorder: " + _spawnBorder);
+    	
     }
 }
