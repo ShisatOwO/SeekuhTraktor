9a3d7a53d62d2403435efbe0cbb0bea9498d9516
Orange-Utan <frowintmp@gmail.com>
2023-08-14 12:02:06 +0200

    HallOfFame implemented

---
+++ Assets/Scripts/HallOFame/hgScoresHoF.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+using System.Collections;
+
+public class hgScoresHoF : MonoBehaviour {
+
+    const string privateCode = "iCS1qBjNs0CGanSPUKy9BAwel91EY91EaywYkdykd-ZQ";
+    const string publicCode = "6055c6188f40bbaf006fd3f0";
+    const string webURL = "http://dreamlo.com/lb/";
+
+    DisplayHighscoresHoF highscoreDisplay;
+    public Highscore[] highscoresList;
+    static hgScoresHoF instance;
+    public string nname;
+    public GameObject nscore;
+    public TMPro.TextMeshProUGUI tmproNscore;
+    public string nscoreString;
+    public int nscoreInt;
+
+    
+    void Awake() {
+
+        /*nscore = this.gameObject.transform.GetChild (0).gameObject;
+        tmproNscore = nscore.GetComponent<TMPro.TextMeshProUGUI>();
+        nscoreString = tmproNscore.text;
+        int.TryParse(nscoreString, out nscoreInt);
+        //Debug.Log(nscoreInt);*/
+        highscoreDisplay = gameObject.GetComponent<DisplayHighscoresHoF> ();
+        instance = this;
+    }
+
+    public static void AddNewHighscore(string username, int score) {
+        instance.StartCoroutine(instance.UploadNewHighscore(username,score));
+    }
+
+    IEnumerator UploadNewHighscore(string username, int score) {
+        WWW www = new WWW(webURL + privateCode + "/add/" + WWW.EscapeURL(username) + "/" + score);
+        yield return www;
+
+        if (string.IsNullOrEmpty(www.error)) {
+            print ("Upload Successful");
+            DownloadHighscores();
+        }
+        else {
+            print ("Error uploading: " + www.error);
+        }
+    }
+
+    public void DownloadHighscores() {
+        StartCoroutine("DownloadHighscoresFromDatabase");
+    }
+
+    IEnumerator DownloadHighscoresFromDatabase() {
+        WWW www = new WWW(webURL + publicCode + "/pipe/");
+        yield return www;
+        
+        if (string.IsNullOrEmpty (www.error)) {
+            FormatHighscores (www.text);
+            highscoreDisplay.OnHighscoresDownloaded(highscoresList);
+        }
+        else {
+            print ("Error Downloading: " + www.error);
+        }
+    }
+
+    void FormatHighscores(string textStream) {
+        string[] entries = textStream.Split(new char[] {'\n'}, System.StringSplitOptions.RemoveEmptyEntries);
+        highscoresList = new Highscore[entries.Length];
+
+        for (int i = 0; i <entries.Length; i ++) {
+            string[] entryInfo = entries[i].Split(new char[] {'|'});
+            string username = entryInfo[0];
+            int score = int.Parse(entryInfo[1]);
+            highscoresList[i] = new Highscore(username,score);
+            print (highscoresList[i].username + ": " + highscoresList[i].score);
+        }
+    }
+
+}
--- Assets/Scripts/SceneSwitcher.cs
+++ Assets/Scripts/SceneSwitcher.cs
@@ -7,8 +7,7 @@ public class SceneSwitcher : MonoBehaviour
 {
     public void toSceneGame() {SceneManager.LoadScene("Game");}
     public void toSceneMenu() {SceneManager.LoadScene("MainMenu");}
-
     public void toSceneHighscore() {SceneManager.LoadScene("HighscoreAfterGame");}
-
     public void toSceneHighscoreMenu() {SceneManager.LoadScene("HighscoreFromMenu");}
+    public void toSceneHallOFame() {SceneManager.LoadScene("HallOFame");}
 }
