69741c641187dcd3e955e8e66b03586d7346986d
Orange-Utan <frowintmp@gmail.com>
2023-08-16 19:49:33 +0200

    Update EveryoneSavesTheDay.cs

--- Assets/Scripts/MediaMark/EveryoneSavesTheDay.cs
+++ Assets/Scripts/MediaMark/EveryoneSavesTheDay.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;  
 using Dialog;
 
 public class EveryoneSavesTheDay : TextLine
 {
     public TextLine nextLineII;
+    public GameObject Main;
+    public GameObject lives;
     private bool dsoo = true;
     private int _start = 0;
     public GameObject TextBox;
     public GameObject TextBoxII;
     public GameObject BoundEndController;
+    public GameObject RaketenTraktor;
 
     // Start is called before the first frame update
     public override void speak()
@@ -18,7 +22,11 @@ public class EveryoneSavesTheDay : TextLine
         _start += 1;
         TextBox.SetActive(false);
         TextBoxII.SetActive(false);
-        if(_start == 2) BoundEndController.GetComponent<BoundEndController>().ActivateEnd();
+        if(_start == 2) {
+            BoundEndController.GetComponent<BoundEndController>().ActivateEnd();
+            PlayerPrefs.SetInt("ScoreSceneOverdub", Main.GetComponent<Vars>().score);
+            PlayerPrefs.SetInt("Lives", lives.GetComponent<live_handler>().GetLives());
+        }
     }
 
     // Update is called once per frame
@@ -30,6 +38,13 @@ public class EveryoneSavesTheDay : TextLine
         if(dsoo && _start == 1 && gameObject.transform.position.x > -5) {
             TextBoxII.SetActive(true);
             dsoo = false;
+        }
+
+        if(_start == 2) {
+            RaketenTraktor.gameObject.transform.position += new Vector3(0,0.1f,0);
+            if(RaketenTraktor.gameObject.transform.position >= 8) {
+                SceneManager.LoadScene("SkyNew"); 
+            }
         }
     }
 }
